aa123aad45b1d8873c94d8034d63005a40443f6a
Monique-Silva <monique.silva-ximenes@le-campus-numerique.fr>
2023-08-09 10:45:01 +0200

    creation de la fonction showInfo avec des getters

--- Jeu_RPG/Menu.java
+++ Jeu_RPG/Menu.java
@@ -4,137 +4,142 @@ public class Menu {
     private String keyboardScan;
     public Warrior warrior = null;
     public Wizard wizard = null;
+    public Weapon weapon = null;
+    public Spell spell = null;
     public String characterChosen;
 
-    public String startGame() {
+    public void startGame() {
         System.out.println("Welcome to the Dungeons and Dragons adventure!");
         System.out.println("Should we begin?");
         do {
             this.showMenu();
-        if (keyboardScan.equals("C")) {
-            System.out.println("Ok! Let's create your character!");
-            characterChosen = this.chooseCharacter(); /** instanciar objetos dentro dessa função */
+            if (keyboardScan.equals("C")) {
+                System.out.println("Ok! Let's create your character!");
+                characterChosen = this.chooseCharacter(); /** instanciar objetos dentro dessa função */
 
-        } else if (keyboardScan.equals("S")) {
-            /** criar função this.showInfo(); */
-        } else if (keyboardScan.equals("M")) {
-            System.out.println(characterChosen); //appeler la function modifier character
+            } else if (keyboardScan.equals("S")) {
+                this.showInfo(characterChosen);
+            } else if (keyboardScan.equals("M")) {
+                System.out.println(characterChosen); //appeler la function modifier character
             } else if (keyboardScan.equals("Q")) {
                 toQuit();
             }
-            return keyboardScan;
-        } while(!(keyboardScan.equals("Q")));
-        }
+        } while (!(keyboardScan.equals("Q")));
+    }
 
-        public void showMenu() {
-            do {
-                System.out.println("Please choose an option:");
-                System.out.println("Create new character(C)");
-                System.out.println("Show character information(S)");
-                System.out.println("Modify your character(M)");
-                System.out.println("Play(P)");
-                System.out.println("Quit(Q)");
-                keyboardScan = scanKeyboard();
-            } while (!(keyboardScan.equals("C") || keyboardScan.equals("S") || keyboardScan.equals("M") || keyboardScan.equals("P") || keyboardScan.equals("Q")));
-        }
-        public String scanKeyboard() {
-            String optionChosen;
+    public void showMenu() {
+        do {
+            System.out.println("Please choose an option:");
+            System.out.println("Create new character(C)");
+            System.out.println("Show character information(S)");
+            System.out.println("Modify your character(M)");
+            System.out.println("Play(P)");
+            System.out.println("Quit(Q)");
+            keyboardScan = scanKeyboard();
+        } while (!(keyboardScan.equals("C") || keyboardScan.equals("S") || keyboardScan.equals("M") || keyboardScan.equals("P") || keyboardScan.equals("Q")));
+    }
+
+    public String scanKeyboard() {
+        String optionChosen;
+        Scanner keyboard = new Scanner(System.in);
+        optionChosen = keyboard.nextLine().toUpperCase();
+        return optionChosen;
+    }
+
+    public String chooseCharacter() {
+        String character;
+        do {
+            System.out.println("Choose your character:");
+            System.out.println("Warrior(WR)");
+            System.out.println("Wizard(WZ)");
+            System.out.println("Quit(Q)");
             Scanner keyboard = new Scanner(System.in);
-            optionChosen = keyboard.nextLine().toUpperCase();
-            return optionChosen;
-        }
+            character = keyboard.nextLine().toUpperCase();
+        } while (!(character.equals("WR") || character.equals("WZ") || character.equals("Q")));
+        if (character.equals("WR")) {
+            warrior = new Warrior();
+            character = "Warrior";
+            System.out.println("A " + character + "? Good choice!");
+            this.createWarrior(warrior, characterChosen);
+        } else if (character.equals("WZ")) {
+            wizard = new Wizard();
+            character = "Wizard";
+            System.out.println("A " + character + "? Good choice!");
+            this.createWizard(wizard);
 
-        public String chooseCharacter () {
-            String character;
-            do {
-                System.out.println("Choose your character:");
-                System.out.println("Warrior(WR)");
-                System.out.println("Wizard(WZ)");
-                System.out.println("Quit(Q)");
-                Scanner keyboard = new Scanner(System.in);
-                character = keyboard.nextLine().toUpperCase();
-            } while (!(character.equals("WR") || character.equals("WZ") || character.equals("Q")));
-            if (character.equals("WR")) {
-                warrior = new Warrior();
-                character = "Warrior";
-                System.out.println("A " + character + "? Good choice!");
-                this.createWarrior(warrior);
-            } else if (character.equals("WZ")) {
-                wizard = new Wizard();
-                character = "Wizard";
-                System.out.println("A " + character + "? Good choice!");
-                this.createWizard(wizard);
-
-            } else if (character.equals("Q")) {
-                toQuit();
-            }
-            return character;
+        } else if (character.equals("Q")) {
+            toQuit();
         }
+        return character;
+    }
 
-        public void createWarrior (Warrior warrior){
-            //String warriorWeapon = warrior.getWeapon();
-            //String warriorShield = warrior.getShield();
-            System.out.println("How do you want to call your warrior?");
-            keyboardScan = scanKeyboard();
-            warrior.setWarriorName(keyboardScan);
-            String warriorName = warrior.getWarriorName();
-            System.out.println("Choose your life points (5-10):");
-            keyboardScan = scanKeyboard();
-            warrior.setWarriorLife(Integer.parseInt(keyboardScan));
-            int warriorLife = warrior.getWarriorLife();
-            System.out.println("Choose your attack points (5-10):");
-            keyboardScan = scanKeyboard();
-            warrior.setWarriorAttack(Integer.parseInt(keyboardScan));
-            int warriorAttack = warrior.getWarriorAttack();
-            System.out.println("Welcome to life, " + warriorName + "! Are you ready to start?");
-            System.out.println("Let's go!");
-            System.out.println("This is you:");
-            System.out.println("Name: " + warriorName);
-            System.out.println("Life points: " + warriorLife);
-            System.out.println("Attack points: " + warriorAttack);
-            //System.out.println("Weapon: " + warriorWeapon);
-            //System.out.println("Filter: " + warriorShield);
-            //menuShowInfo();
-        }
+    public void createWarrior(Warrior warrior, String characterChosen) {
+        System.out.println("How do you want to call your warrior?");
+        keyboardScan = scanKeyboard();
+        warrior.setWarriorName(keyboardScan);
+        String warriorName = warrior.getWarriorName();
+        System.out.println("Choose your life points (5-10):");
+        keyboardScan = scanKeyboard();
+        warrior.setWarriorLife(Integer.parseInt(keyboardScan));
+        warrior.getWarriorLife();
+        System.out.println("Choose your attack points (5-10):");
+        keyboardScan = scanKeyboard();
+        warrior.setWarriorAttack(Integer.parseInt(keyboardScan));
+        warrior.getWarriorAttack();
+        System.out.println("Welcome to life, " + warriorName + "! Are you ready to start?");
+        System.out.println("Let's go!");
+    }
 
-        public void createWizard (Wizard wizard){
-            String wizardName = wizard.getWizardName();
-            int wizardLife = wizard.getWizardLife();
-            int wizardAttack = wizard.getWizardAttack();
-            String wizardSpell = wizard.spell.getSpell();
-            String wizardFilter = wizard.spell.getFilter();
-            System.out.println("How do you want to call your wizard?");
-            keyboardScan = scanKeyboard();
-            wizardName = keyboardScan;
-            System.out.println("Choose your life points (3-6):");
-            keyboardScan = scanKeyboard();
-            wizardLife = Integer.parseInt(keyboardScan);
-            System.out.println("Choose your attack points (8-15):");
-            keyboardScan = scanKeyboard();
-            wizardAttack = Integer.parseInt(keyboardScan);
-            System.out.println("Welcome to life, " + wizardName + "! Are you ready to start?");
-            System.out.println("Let's go!");
-            System.out.println("This is you:");
-            System.out.println("Name: " + wizardName);
-            System.out.println("Life points: " + wizardLife);
-            System.out.println("Attack points: " + wizardAttack);
-            System.out.println("Spell: " + wizardSpell);
-            System.out.println("Filter: " + wizardFilter);
-            //menuShowInfo();
-        }
+    public void createWizard(Wizard wizard) {
+        String wizardName = wizard.getWizardName();
+        int wizardLife = wizard.getWizardLife();
+        int wizardAttack = wizard.getWizardAttack();
+        String wizardSpell = wizard.spell.getSpell();
+        String wizardFilter = wizard.spell.getFilter();
+        System.out.println("How do you want to call your wizard?");
+        keyboardScan = scanKeyboard();
+        wizardName = keyboardScan;
+        System.out.println("Choose your life points (3-6):");
+        keyboardScan = scanKeyboard();
+        wizardLife = Integer.parseInt(keyboardScan);
+        System.out.println("Choose your attack points (8-15):");
+        keyboardScan = scanKeyboard();
+        wizardAttack = Integer.parseInt(keyboardScan);
+        System.out.println("Welcome to life, " + wizardName + "! Are you ready to start?");
+        System.out.println("Let's go!");
+        this.showInfo(characterChosen);
+    }
 
-        public void toQuit () {
-            System.out.println("Are you sure you want to quit (Y/N)?");
-            Scanner keyboard2 = new Scanner(System.in);
-            keyboardScan = keyboard2.nextLine().toUpperCase();
-            switch (keyboardScan) {
-                case "Y":
-                    System.out.println("Ok, see you next time!");
-                    System.exit(0);
-                case "N":
-                    System.out.println("Ok, let's start from the beginning.");
-                    keyboardScan = startGame();
-            }
+    public void toQuit() {
+        System.out.println("Are you sure you want to quit (Y/N)?");
+        Scanner keyboard2 = new Scanner(System.in);
+        keyboardScan = keyboard2.nextLine().toUpperCase();
+        switch (keyboardScan) {
+            case "Y":
+                System.out.println("Ok, see you next time!");
+                System.exit(0);
+            case "N":
+                System.out.println("Ok, let's start from the beginning.");
+                this.startGame();
         }
+    }
+
+    public void showInfo(String characterChosen) {
+        System.out.println("This is you:");
+        switch (characterChosen) {
+            case "Warrior":
+                System.out.println("Name: " + warrior.getWarriorName());
+                System.out.println("Life points: " + warrior.getWarriorLife());
+                System.out.println("Attack points: " + warrior.getWarriorAttack());
+                //System.out.println("Weapon: " + warrior.getWeapon());
+                //System.out.println("Shield: " + warrior.getShield());
+            case "Wizard":
+                System.out.println("Name: " + wizard.getWizardName());
+                System.out.println("Life points: " + wizard.getWizardLife());
+                System.out.println("Attack points: " + wizard.getWizardAttack());
+                //System.out.println("Spell: " + wizard.getSpell());
+                //System.out.println("Filter: " + wizard.getFilter());
+        }
+    }
     }
 
